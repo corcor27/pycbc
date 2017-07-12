44edfe57cd164cb7eea748a494b7a165046642fb
corcor27 <cory13579@googlemail.com>
2017-07-12 14:37:31 +0100

    Update test_pycbc_inference.sh

--- test_pycbc_inference.sh
+++ test_pycbc_inference.sh
@@ -8,7 +8,7 @@ PAR=${NAMEDIR}/parameters.txt
 # define coalescence time, observed masses, and waveform parameters
 TRIGGER_TIME=1126259462.0
 INJ_APPROX=IMRPhenomPv2threePointFivePN
-MASS1=40.
+MASS1=80.
 MASS2=10.
 RA=2.21535724066
 DEC=-1.23649695537
